82d785bec899560aec3585cf93932e91f48d4fd9
Spencer King <spencer@spencertking.com>
2021-10-30 20:33:47 -0500

    Basic build process working

--- gregorian-mode.el
+++ gregorian-mode.el
@@ -35,6 +35,7 @@
   )
 
 (defun gregorian-create-new-gabc()
+  ; Create a new gabc file
   (interactive)
   (setq f (read-string "Enter file name:"))
   (if (not (string= (car (last (split-string f "\\."))) "gabc"))
@@ -43,6 +44,42 @@
       (message (concat  "File " (concat f " already exists")))
     (with-temp-buffer (write-file f))
     (find-file f)))
+
+(defun gregorian-create-tex-template(fname)
+  ; Create a generic LaTeX template for the score
+  (setq outfile (concat fname ".tex"))
+  (with-temp-buffer (write-file outfile))
+  (append-to-file "\\documentclass{scrartcl}\n" nil outfile)
+  (append-to-file "\\usepackage[osf,p]{libertine}\n" nil outfile)
+  (append-to-file "\\usepackage{gregoriotex}\n" nil outfile)
+  (append-to-file "\\usepackage[latin]{babel}\n" nil outfile)
+  (append-to-file "\\setkomafont{section}{\\normalfont\\centering\\huge\\scshape}\n" nil outfile)
+  (append-to-file "\\setcounter{secnumdepth}{-\\maxdimen}\n" nil outfile)
+  (append-to-file "\\grechangedim{beforeinitialshift}{2.2mm}{scalable}\n" nil outfile)
+  (append-to-file "\\grechangedim{afterinitialshift}{2.2mm}{scalable}\n" nil outfile)
+  (append-to-file "\\grechangestyle{initial}{\\fontsize{43}{43}\\selectfont}\n" nil outfile)
+  (append-to-file "\\gresetlinecolor{gregoriocolor}\n" nil outfile)
+  (append-to-file "\\gresetheadercapture{commentary}{grecommentary}{string}\n" nil outfile)
+  (append-to-file "\\grechangestyle{annotation}{\\small\\bfseries}\n" nil outfile)
+  (append-to-file "\\begin{document}\n" nil outfile)
+  (setq score (concat "\\gregorioscore[a]{" (file-name-base fname) "}\n"))
+  (append-to-file score  nil outfile)
+  (append-to-file "\\end{document}" nil outfile))
+
+(defun gregorian-build()
+  ; Build the score
+  (interactive)
+  (setq f (file-name-nondirectory buffer-file-name))
+  (setq fname (file-name-sans-extension f))
+  (setq ext (file-name-extension f))
+  (if (not (string= "gabc" ext))
+      (print "not gabc")
+    (shell-command (concat "gregorio " f))
+    (gregorian-create-tex-template (file-name-sans-extension buffer-file-name))
+    (shell-command (concat "lualatex --shell-escape " (file-name-base buffer-file-name) ".tex"))
+    ;(print "gabc")
+    )
+  )
 
 (provide 'gregorian-mode)
 
